773506250c82a4cc4a0d961797099811adccfd91
frankiejgit <frankiejgram@gmail.com>
2024-06-24 01:24:22 -0400

    adding AGPL license header

--- code/ui/data_tab.R
+++ code/ui/data_tab.R
@@ -1,3 +1,21 @@
+# CHiDO is a no-code platform to integrate multi-omics data to build, train and test
+# linear mixed models for identifying candidates for desired GxE interactions.
+#
+# Copyright (C) 2024 Francisco Gonzalez, Diego Jarquin, and Julian Garcia
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # Created by: Francisco Gonzalez
 # Last Updated By: Francisco Gonzalez
 # Last Updated: 06/10/2024
